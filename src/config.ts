142e2d12a3fee87fa0bb978a3d77464fe99098b1
neemiasmartins <86680482+neemiasmartins@users.noreply.github.com>
2026-02-26 15:16:55 +0000

    index page

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Neemias Martins';
-export const SITE_DESCRIPTION = 'Neemias Martins | Dynamical Systems and Ergodic Theory';
+export const SITE_TITLE = 'Vaccari Consultoria';
+export const SITE_DESCRIPTION = 'Vaccari Consultoria | Planejamento Estratégico, Organização de Processos e Estruturação Comercial';
